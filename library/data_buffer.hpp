43fca3055785cb30f2e3f024930a192f3bba507a
SimonCROS <scros@student.42lyon.fr>
2024-11-15 19:12:23 +0100

    Start data buffer

--- library/data_buffer.hpp
+++ library/data_buffer.hpp
@@ -4,13 +4,35 @@
 
 #ifndef DATA_BUFFER_HPP
 #define DATA_BUFFER_HPP
+#include <cstdint>
+#include <streambuf>
+#include <vector>
 
+class DataBuffer
+{
+public:
+    class DataDeserializationException final : public std::exception
+    {
+        [[nodiscard]] const char* what() const noexcept override
+        {
+            return "Failed to retrieve data from buffer";
+        }
+    };
 
+    template <typename TType>
+    auto operator<<(const TType& rhs) -> DataBuffer&
+    {
+        mBuffer << rhs;
+        //
+        // auto data = static_cast<const void*>(&rhs);
+        // bytes.insert(bytes.end(), data, data + sizeof(TType));
+        return *this;
+    }
 
-class DataBuffer {
+    auto operator>>(int& rhs) -> DataBuffer&;
 
+private:
+    std::vector<uint8_t> bytes;
 };
-
-
 
 #endif //DATA_BUFFER_HPP
---
+++ library/data_structures.hpp
@@ -0,0 +1,11 @@
+//
+// Created by scros on 11/15/24.
+//
+
+#ifndef DATA_STRUCTURES_HPP
+#define DATA_STRUCTURES_HPP
+
+#include "data_buffer.hpp"
+#include "pool.hpp"
+
+#endif //DATA_STRUCTURES_HPP
