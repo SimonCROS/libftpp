d0d42a0ec02331817b00c8233dcb5c42f838eb6b
Simon CROS <simon.dc.cros@gmail.com>
2024-11-16 01:48:55 +0100

    data_buffer is working

--- library/data_buffer.cpp
+++ library/data_buffer.cpp
@@ -3,3 +3,43 @@
 //
 
 #include "data_buffer.hpp"
+
+#include <cassert>
+
+auto DataBuffer::pushBytes(const void* data, const std::size_t size) -> void
+{
+    assert(data != nullptr);
+
+    if (size > max_push_size)
+        throw DataDeserializationException();
+
+    auto ptr = static_cast<const uint8_t*>(data);
+    mBytes.insert(mBytes.end(), ptr, ptr + size);
+}
+
+auto DataBuffer::frontBytes(const std::size_t size) -> std::ranges::subrange<decltype(mBytes)::const_iterator>
+{
+    if (size > max_push_size)
+        throw DataDeserializationException();
+
+    if (mBytes.size() < size)
+        throw DataDeserializationException();
+
+    return std::ranges::subrange(mBytes.begin(), mBytes.begin() + static_cast<decltype(mBytes)::difference_type>(size));
+}
+
+auto DataBuffer::unshiftBytes(const std::size_t size, void* out) -> void
+{
+    if (size > max_push_size)
+        throw DataDeserializationException();
+
+    if (mBytes.size() < size)
+        throw DataDeserializationException();
+
+    if (out != nullptr)
+    {
+        auto ptr = static_cast<uint8_t*>(out);
+        std::copy_n(mBytes.begin(), size, ptr);
+    }
+    mBytes.erase(mBytes.begin(), mBytes.begin() + static_cast<decltype(mBytes)::difference_type>(size));
+}
--- library/data_buffer.hpp
+++ library/data_buffer.hpp
@@ -5,12 +5,20 @@
 #ifndef DATA_BUFFER_HPP
 #define DATA_BUFFER_HPP
 #include <cstdint>
+#include <deque>
+#include <optional>
 #include <streambuf>
 #include <vector>
+#include <string_view>
 
 class DataBuffer
 {
+private:
+    std::deque<uint8_t> mBytes;
+
 public:
+    static constexpr auto max_push_size = std::numeric_limits<decltype(mBytes)::difference_type>::max();
+
     class DataDeserializationException final : public std::exception
     {
         [[nodiscard]] const char* what() const noexcept override
@@ -19,20 +27,43 @@ public:
         }
     };
 
-    template <typename TType>
-    auto operator<<(const TType& rhs) -> DataBuffer&
-    {
-        mBuffer << rhs;
-        //
-        // auto data = static_cast<const void*>(&rhs);
-        // bytes.insert(bytes.end(), data, data + sizeof(TType));
-        return *this;
-    }
+    auto pushBytes(const void *data, std::size_t size) -> void;
 
-    auto operator>>(int& rhs) -> DataBuffer&;
+    auto frontBytes(std::size_t size) -> std::ranges::subrange<decltype(mBytes)::const_iterator>;
 
-private:
-    std::vector<uint8_t> bytes;
+    auto unshiftBytes(std::size_t size, void* out) -> void;
 };
+
+template<class T>
+requires std::is_integral_v<T>
+auto operator<<(DataBuffer& buffer, const T& value) -> DataBuffer&
+{
+    buffer.pushBytes(&value, sizeof(T));
+    return buffer;
+}
+
+template<class T>
+requires std::is_integral_v<T>
+auto operator>>(DataBuffer& buffer, T& value) -> DataBuffer&
+{
+    buffer.unshiftBytes(sizeof(T), &value);
+    return buffer;
+}
+
+inline auto operator<<(DataBuffer& buffer, const std::string_view& value) -> DataBuffer&
+{
+    buffer << value.size();
+    buffer.pushBytes(value.data(), value.size());
+    return buffer;
+}
+
+inline auto operator>>(DataBuffer& buffer, std::string& value) -> DataBuffer&
+{
+    size_t size = 0;
+    buffer >> size;
+    value.assign_range(buffer.frontBytes(size));
+    buffer.unshiftBytes(size, nullptr);
+    return buffer;
+}
 
 #endif //DATA_BUFFER_HPP
