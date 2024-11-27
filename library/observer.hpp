897b93ed77d5de4cabd106053c67e8166ed17ef4
SimonCROS <scros@student.42lyon.fr>
2024-11-27 13:04:59 +0100

    Add observer

--- library/observer.hpp
+++ library/observer.hpp
@@ -4,22 +4,37 @@
 
 #ifndef OBSERVER_HPP
 #define OBSERVER_HPP
+#include <unordered_map>
+#include <vector>
 #include <functional>
 
 template <class TEvent>
 class Observer
 {
+    using container_type = std::unordered_map<TEvent, std::vector<std::function<void()>>>;
+
 public:
     void subscribe(const TEvent& event, const std::function<void()>& lambda)
     {
+        typename container_type::iterator it;
+        std::tie(it, std::ignore) = m_observers.try_emplace(event);
+        it->second.push_back(lambda);
     }
 
     void notify(const TEvent& event)
     {
+        const auto it = m_observers.find(event);
+        if (it != m_observers.end())
+        {
+            for (const auto& element : it->second)
+            {
+                element();
+            }
+        }
     }
 
 private:
-    std::unordered_map<TEvent, std::vector<std::function<void()>>> m_observers;
+    container_type m_observers;
 };
 
 #endif //OBSERVER_HPP
