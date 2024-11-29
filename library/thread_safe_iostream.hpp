368382f96198759c113aa5b55e0998e892a8e0d0
SimonCROS <scros@student.42lyon.fr>
2024-11-29 19:57:38 +0100

    TMP Start edit iostream

--- library/thread_safe_iostream.hpp
+++ library/thread_safe_iostream.hpp
@@ -7,18 +7,19 @@
 #include <mutex>
 #include <string>
 #include <iostream>
+#include <sstream>
 
 #include "prefixed_ostream.hpp"
 
 template <class T>
-concept Printable = requires(T value)
+concept ThreadSafePrintable = requires(T value)
 {
-    std::cout << value;
+    std::declval<std::ostringstream>() << value;
     !std::is_same_v<T, std::ostream& (*)(std::ostream&)>;
 };
 
 template <class T>
-concept Readable = requires(T value)
+concept ThreadSafeReadable = requires(T value)
 {
     std::cin >> value;
 };
@@ -28,6 +29,8 @@ class ThreadSafeIOStream
     inline static std::mutex ms_writeMutex;
     inline static std::mutex ms_readMutex;
 
+    std::ostringstream m_stream;
+
     PrefixedOStream m_prefixedStdout{std::cout};
 
 public:
@@ -35,15 +38,18 @@ public:
 
     auto operator<<(std::ostream& (*manip)(std::ostream&)) -> ThreadSafeIOStream&; // required to accept endl
 
-    template <Printable T>
+    template <ThreadSafePrintable T>
     auto operator<<(const T& value) -> ThreadSafeIOStream&
     {
         std::lock_guard lock(ms_writeMutex);
-        m_prefixedStdout << value;
+        m_stream << value;
+
+
+        // m_prefixedStdout << value;
         return *this;
     }
 
-    template <Readable T>
+    template <ThreadSafeReadable T>
     auto operator>>(T& value) -> ThreadSafeIOStream&
     {
         std::lock_guard lock(ms_readMutex);
