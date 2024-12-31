c33ae05fb73b3fd957c37a060a9304db544d2260
SimonCROS <scros@student.42lyon.fr>
2024-12-31 18:40:41 +0100

    Change TType with T, ::type with ::value_type

--- library/observable_value.hpp
+++ library/observable_value.hpp
@@ -9,57 +9,57 @@
 #include <functional>
 #include <vector>
 
-template <class TType>
+template <class T>
 class ObservableValue
 {
 public:
-    using type = TType;
+    using value_type = T;
 
 private:
-    using listener_type = std::function<void (const TType&)>;
+    using listener_type = std::function<void (const T&)>;
     using listeners_list_type = std::vector<listener_type>;
 
-    TType m_value;
+    T m_value;
     listeners_list_type m_listeners;
 
 public:
     constexpr ObservableValue()
         noexcept(
-            std::is_nothrow_default_constructible_v<TType> &&
+            std::is_nothrow_default_constructible_v<T> &&
             std::is_nothrow_default_constructible_v<listeners_list_type>)
         requires
-        std::default_initializable<TType>
+        std::default_initializable<T>
         : m_value{}
     {
     }
 
     constexpr ObservableValue(const ObservableValue& other) = delete;
 
-    constexpr ObservableValue(ObservableValue&& other) noexcept(std::is_nothrow_move_constructible_v<TType> &&
+    constexpr ObservableValue(ObservableValue&& other) noexcept(std::is_nothrow_move_constructible_v<T> &&
         std::is_nothrow_move_constructible_v<listeners_list_type>) requires std::move_constructible<
-        TType> : m_value{std::move(other.m_value)}, m_listeners{std::move(other.m_listeners)}
+        T> : m_value{std::move(other.m_value)}, m_listeners{std::move(other.m_listeners)}
     {
     }
 
     template <class... Args>
-        requires std::constructible_from<TType, Args...>
+        requires std::constructible_from<T, Args...>
     constexpr explicit ObservableValue(std::in_place_t, Args&&... args) : m_value{std::forward<Args>(args)...}
     {
     }
 
     template <class U, class... Args>
-        requires std::constructible_from<TType, std::initializer_list<U>, Args...>
+        requires std::constructible_from<T, std::initializer_list<U>, Args...>
     constexpr explicit ObservableValue(std::in_place_t, std::initializer_list<U> ilist, Args&&... args)
         : m_value{ilist, std::forward<Args>(args)...}
     {
     }
 
-    template <class U = TType>
+    template <class U = T>
         requires
-        std::constructible_from<TType, U&&> &&
+        std::constructible_from<T, U&&> &&
         (!std::same_as<std::remove_cvref<U>, std::in_place_t>) &&
         (!std::same_as<std::remove_cvref<U>, ObservableValue>)
-    explicit(std::convertible_to<TType, U&&>)
+    explicit(std::convertible_to<T, U&&>)
     constexpr ObservableValue(U&& value) : m_value(std::forward<U>(value))
     {
     }
--- tests/main_observable_value.cpp
+++ tests/main_observable_value.cpp
@@ -10,6 +10,7 @@
 #include "thread_safe_iostream.hpp"
 
 struct MyType {
+    MyType();
     MyType(int x) { std::cout << "Single int constructor\n"; }
     MyType(std::initializer_list<int> il) { std::cout << "Initializer list constructor\n"; }
 };
@@ -20,9 +21,11 @@ int main()
     std::optional<MyType> opt1{1};
     // std::optional<MyType> opt2{{1, 2, 3}};
 
+    ObservableValue<MyType> ov0;
     ObservableValue<MyType> ov1{std::in_place, 1};
     ObservableValue<MyType> ov2{std::in_place, {1, 2, 3}};
     ObservableValue<MyType> ov3{{1, 2, 3}};
+    ObservableValue<MyType> ov4{MyType(4)};
 
     return 0;
 }
