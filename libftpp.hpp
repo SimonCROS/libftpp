ae9d369f320612b084cad63ce5efa96e6412919c
Simon CROS <simon.dc.cros@gmail.com>
2025-01-01 22:56:41 +0100

    Add libftpp.hpp, fix output .a name

---
+++ libftpp.hpp
@@ -0,0 +1,17 @@
+//
+// Created by Simon Cros on 01/01/2025.
+//
+
+#ifndef LIBFTPP_HPP
+#define LIBFTPP_HPP
+
+#include "library/data_structures.hpp"
+#include "library/design_patterns.hpp"
+#include "library/thread_safe_iostream.hpp"
+#include "library/threading.hpp"
+#include "library/network.hpp"
+#include "library/mathematics.hpp"
+#include "library/chrono.hpp"
+#include "library/observable_value.hpp"
+
+#endif //LIBFTPP_HPP
