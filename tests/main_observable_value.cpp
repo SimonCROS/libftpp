72f3815885a03eb96961e39fe9be798da4e3debc
Simon CROS <simon.dc.cros@gmail.com>
2025-01-01 18:49:44 +0100

    ObservableValue test all constructors compilation

--- tests/main_observable_value.cpp
+++ tests/main_observable_value.cpp
@@ -9,23 +9,21 @@
 
 #include "thread_safe_iostream.hpp"
 
-struct MyType {
-    MyType();
+struct MyType
+{
+    MyType() = default;
     MyType(int x) { std::cout << "Single int constructor\n"; }
     MyType(std::initializer_list<int> il) { std::cout << "Initializer list constructor\n"; }
 };
 
 int main()
 {
-    // Use std::optional to wrap MyType
-    std::optional<MyType> opt1{1};
-    // std::optional<MyType> opt2{{1, 2, 3}};
-
-    ObservableValue<MyType> ov0;
-    ObservableValue<MyType> ov1{std::in_place, 1};
-    ObservableValue<MyType> ov2{std::in_place, {1, 2, 3}};
-    ObservableValue<MyType> ov3{{1, 2, 3}};
-    ObservableValue<MyType> ov4{MyType(4)};
+    ObservableValue<MyType> ov1{}; //                               (1)
+    ObservableValue<MyType> ov2{std::move(ov1)}; //             (2)
+    ObservableValue<MyType> ov3{std::in_place, 1}; //               (3)
+    ObservableValue<MyType> ov4{std::in_place, {1, 2, 3}}; //   (4)
+    ObservableValue<MyType> ov5{{1, 2, 3}}; //                   (5)
+    ObservableValue<MyType> ov6{MyType(4)}; //                    (5)
 
     return 0;
 }
